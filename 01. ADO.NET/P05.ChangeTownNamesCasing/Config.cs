6157aa72a42e2faa0cde59b199008bfcb45bbbb4
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-26 15:26:51 +0300

    Added P05.ChangeTownNamesCasing

---
+++ 01. ADO.NET/P05.ChangeTownNamesCasing/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P05.ChangeTownNamesCasing
+{
+    internal class Config
+    {
+        public const string ConnectionString =
+            @"Server=DIMITARPC;DATABASE=MinionsDB;Integrated Security=True;Trust Server Certificate=True";
+    }
+}
---
+++ 01. ADO.NET/P05.ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Text;
+
+namespace P05.ChangeTownNamesCasing
+{
+    internal class StartUp
+    {
+        static async Task Main(string[] args)
+        {
+            await using SqlConnection connection =
+                new SqlConnection(Config.ConnectionString);
+            connection.Open();
+
+            string nameOfCountry = Console.ReadLine();
+            string result = await GetOutput(connection, nameOfCountry);
+
+            Console.WriteLine(result);
+        }
+
+        static async Task<string> GetOutput(SqlConnection connection, string country)
+        {
+            int rowsAffected = await UpdateTownNamesOfACountry(connection, country);
+
+            SqlCommand getOutputCmd = new SqlCommand(@"
+                SELECT t.Name 
+                FROM Towns as t
+                    JOIN Countries AS c 
+                    ON c.Id = t.CountryCode
+                WHERE c.Name = @countryName", connection);
+            getOutputCmd.Parameters.AddWithValue("@countryName", country);
+
+            SqlDataReader townsReader = await getOutputCmd.ExecuteReaderAsync();
+
+            StringBuilder sb = new StringBuilder();
+
+            if (!townsReader.HasRows)
+            {
+                return "No town names were affected.";
+            }
+            else
+            {
+                sb.AppendLine($"{rowsAffected} town names were affected.");
+                List<string> townsAffected = new List<string>();
+
+                while (townsReader.Read())
+                {
+                    string townName = (string)townsReader["Name"];
+                    townsAffected.Add(townName);
+                }
+
+                sb.AppendLine($"[{string.Join(", ", townsAffected)}]");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        static async Task<int> UpdateTownNamesOfACountry(SqlConnection connection, string country)
+        {
+            SqlCommand updateTownNameCmd = new SqlCommand(@"
+                UPDATE Towns
+                SET Name = UPPER(Name)
+                WHERE CountryCode = (
+                    SELECT c.Id 
+                    FROM Countries AS c 
+                    WHERE c.Name = @countryName)", connection);
+            updateTownNameCmd.Parameters.AddWithValue("@countryName", country);
+
+            return await updateTownNameCmd.ExecuteNonQueryAsync();
+        }
+    }
+}
