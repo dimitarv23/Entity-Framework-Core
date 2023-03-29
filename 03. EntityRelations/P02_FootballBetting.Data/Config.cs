6a945a321f461b34149261cdfdd37532e6ac218b
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-03-29 09:38:38 +0300

    Added P02_FootballBetting_Data

---
+++ 03. EntityRelations/P02_FootballBetting.Data/Config.cs
@@ -0,0 +1,7 @@
+﻿namespace P02_FootballBetting.Data
+{
+    public class Config
+    {
+        public const string ConnectionString = "Server=DIMITARPC;Database=Bet377;Integrated Security=True;";
+    }
+}
---
+++ 03. EntityRelations/P02_FootballBetting.Data/FootballBettingContext.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P02_FootballBetting.Data.Models;
+
+namespace P02_FootballBetting.Data
+{
+    public class FootballBettingContext : DbContext
+    {
+        public FootballBettingContext()
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Color> Colors { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<PlayerStatistic> PlayersStatistics { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public FootballBettingContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Config.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<PlayerStatistic>(e =>
+                {
+                    e.HasKey(ps => new { ps.PlayerId, ps.GameId });
+                });
+
+            modelBuilder
+                .Entity<Team>(e =>
+                {
+                    e
+                    .HasOne(t => t.PrimaryKitColor)
+                    .WithMany(c => c.PrimaryKitTeams)
+                    .HasForeignKey(t => t.PrimaryKitColorId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+                    e
+                    .HasOne(t => t.SecondaryKitColor)
+                    .WithMany(c => c.SecondaryKitTeams)
+                    .HasForeignKey(t => t.SecondaryKitColorId)
+                    .OnDelete(DeleteBehavior.NoAction);
+                });
+
+            modelBuilder
+                .Entity<Game>(e =>
+                {
+                    e
+                    .HasOne(g => g.HomeTeam)
+                    .WithMany(t => t.HomeGames)
+                    .HasForeignKey(g => g.HomeTeamId)
+                    .OnDelete(DeleteBehavior.NoAction);
+
+                    e
+                    .HasOne(g => g.AwayTeam)
+                    .WithMany(t => t.AwayGames)
+                    .HasForeignKey(g => g.AwayTeamId)
+                    .OnDelete(DeleteBehavior.NoAction);
+                });
+        }
+    }
+}
