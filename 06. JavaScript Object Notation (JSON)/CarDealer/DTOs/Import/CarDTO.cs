c1ba0c0c1046561557e8d71594df3b19585191eb
Dimitar Vasilev <captaingetrekt@gmail.com>
2023-04-01 11:42:08 +0300

    Added CarDealer project

---
+++ 06. JavaScript Object Notation (JSON)/CarDealer/DTOs/Import/CarDTO.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.DTOs.Import
+{
+    public class CarDTO
+    {
+        [JsonProperty("make")]
+        public string Make { get; set; } = null!;
+
+        [JsonProperty("model")]
+        public string Model { get; set; } = null!;
+
+        [JsonProperty("traveledDistance")]
+        public long TravelledDistance { get; set; }
+
+        [JsonProperty("partsId")]
+        [NotMapped]
+        public List<int> PartsId { get; set; } = null!;
+    }
+}
---
+++ 06. JavaScript Object Notation (JSON)/CarDealer/StartUp.cs
@@ -0,0 +1,208 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using Castle.Core.Resource;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext dbContext = new CarDealerContext();
+
+            var outputJson = GetCarsWithTheirListOfParts(dbContext);
+
+            Console.WriteLine(outputJson);
+            File.WriteAllText("../../../Results/cars-and-parts.json", outputJson);
+        }
+
+        //Exercise 9
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var supplierDTOs = JsonConvert.DeserializeObject<List<SupplierDTO>>(inputJson);
+            var suppliers = mapper.Map<List<Supplier>>(supplierDTOs);
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count}.";
+        }
+
+        //Exercise 10
+        public static string ImportParts(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var partDTOs = JsonConvert.DeserializeObject<List<PartDTO>>(inputJson);
+            var parts = new List<Part>();
+            var validSupplierIds = context.Suppliers
+                .Select(s => s.Id)
+                .ToList(); ;
+
+            foreach (var partDTO in partDTOs)
+            {
+                if (validSupplierIds.Contains(partDTO.SupplierId))
+                {
+                    var part = mapper.Map<Part>(partDTO);
+                    parts.Add(part);
+                }
+            }
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count}.";
+        }
+
+        //Exercise 11
+        public static string ImportCars(CarDealerContext context, string inputJson)
+        {
+            var carsDto = JsonConvert.DeserializeObject<CarDTO[]>(inputJson);
+            var cars = new List<Car>();
+            var carParts = new List<PartCar>();
+
+            foreach (var carDto in carsDto)
+            {
+                var car = new Car
+                {
+                    Make = carDto.Make,
+                    Model = carDto.Model,
+                    TravelledDistance = carDto.TravelledDistance
+                };
+
+                foreach (var part in carDto.PartsId.Distinct())
+                {
+                    var carPart = new PartCar()
+                    {
+                        PartId = part,
+                        Car = car
+                    };
+
+                    carParts.Add(carPart);
+                }
+            }
+
+            context.Cars.AddRange(cars);
+            context.PartsCars.AddRange(carParts);
+            context.SaveChanges();
+
+            return $"Successfully imported {context.Cars.Count()}.";
+        }
+
+        //Exercise 12
+        public static string ImportCustomers(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var customerDTOs = JsonConvert.DeserializeObject<List<CustomerDTO>>(inputJson);
+            var customers = mapper.Map<List<Customer>>(customerDTOs);
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count}.";
+        }
+
+        //Exercise 13
+        public static string ImportSales(CarDealerContext context, string inputJson)
+        {
+            IMapper mapper = CreateMapper();
+
+            var saleDTOs = JsonConvert.DeserializeObject<List<SaleDTO>>(inputJson);
+            var sales = mapper.Map<List<Sale>>(saleDTOs);
+
+            context.Sales.AddRange(sales);
+            context.SaveChanges();
+
+            return $"Successfully imported {sales.Count}.";
+        }
+
+        //Exercise 14
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var customers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    BirthDate = c.BirthDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    IsYoungDriver = c.IsYoungDriver
+                }).ToList();
+
+            return JsonConvert.SerializeObject(customers, Formatting.Indented);
+        }
+
+        //Exercise 15
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Where(c => c.Make == "Toyota")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TravelledDistance)
+                .Select(c => new
+                {
+                    Id = c.Id,
+                    Make = c.Make,
+                    Model = c.Model,
+                    TraveledDistance = c.TravelledDistance
+                }).ToList();
+
+            return JsonConvert.SerializeObject(cars, Formatting.Indented);
+        }
+
+        //Exercise 16
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var suppliers = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    PartsCount = s.Parts.Count
+                }).ToList();
+
+            return JsonConvert.SerializeObject(suppliers, Formatting.Indented);
+        }
+
+        //Exercise 17
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var cars = context.Cars
+                .Select(c => new
+                {
+                    car = new
+                    {
+                        Make = c.Make,
+                        Model = c.Model,
+                        TraveledDistance = c.TravelledDistance
+                    },
+                    parts = c.PartsCars
+                        .Select(pc => new
+                        {
+                            Name = pc.Part.Name,
+                            Price = pc.Part.Price
+                        })
+                }).ToList();
+
+            return JsonConvert.SerializeObject(cars, Formatting.Indented);
+        }
+
+        private static IMapper CreateMapper()
+        {
+            return new Mapper(new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<CarDealerProfile>();
+            }));
+        }
+    }
+}
